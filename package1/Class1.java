924e2affef6583df03086dbfef67a1b73dd3f56d
arjtban <ban.arijit@outlook.com>
2023-03-05 15:24:26 +0530

    Add files via upload

---
+++ package1/Class1.java
@@ -0,0 +1,42 @@
+package package1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Class1 {
+
+	WebDriver driver;
+	public Class1(WebDriver driver) {
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	
+	//WebElement login=driver.findElement(By.id("pt-login-2"));
+
+	//Pagefactory.
+	@FindBy(id="pt-login-2")
+	WebElement login;
+	
+	@FindBy(name="wpName")
+	WebElement username;
+	
+	@FindBy(name="wpPassword")
+	WebElement password;
+	
+	@FindBy(xpath="//button[@id='wpLoginAttempt']")
+	WebElement button;
+	
+	public void gotologin() {
+		login.click();
+		username.sendKeys("ABCD");
+		password.sendKeys("abc@123");
+		button.click();
+	}
+	
+}
---
+++ package1/Class2.java
@@ -0,0 +1,18 @@
+package package1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "/home/arijit/Documents/WebDrivers/chromedriver");	
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://en.wikipedia.org/wiki/Main_Page");
+		Class1 obj1=new Class1(driver);	
+		obj1.gotologin();
+		
+	}
+
+}
